472f2dcbb2f2acf8ba09745fd2b4b828d5a9222b
Tak-kuen <32920482+Tak-kuen@users.noreply.github.com>
2018-09-06 20:36:39 +0900

    db programming

--- code/20180903db프로그래밍/src/WDB.java
+++ code/20180903db프로그래밍/src/WDB.java
@@ -5,6 +5,17 @@ public class WDB {
 		myObject.connectDB();
 		//myObject.insertMember();
 		// 조회
-		myObject.select();
+		//myObject.select();
+		//한 항목에 대한 모든 정보 출력
+		//myObject.retrieve(6);
+		//myObject.allRetrieve();
+		//myObject.printTable();
+		//myObject.deleteBookID(6);
+		myObject.printTable();
+		
+		//PrepareStatement를 이용한 쿼리객체 사용
+		myObject.insert(9,"Nyyame2"
+				+ "","Pub","2018",10000);
+		myObject.printTable();
 	}
 }
