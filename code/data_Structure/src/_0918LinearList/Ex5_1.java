a9d1053525a978b603e0ea455c6dae37a824cad2
Tak-kuen <32920482+Tak-kuen@users.noreply.github.com>
2018-09-18 12:02:31 +0900

    GUI and Data Structure
    20180917,20180918

---
+++ code/data_Structure/src/_0918LinearList/Ex5_1.java
@@ -0,0 +1,11 @@
+package _0918LinearList;
+//1501163 º€≈π±Ÿ
+public class Ex5_1 {
+	public static void main(String[] args) {
+		int sale[] = new int[] {157,209,251,312};
+		
+		for(int i=0 ; i<sale.length ; i++) {
+			System.out.printf("%d/4∫–±‚ : sale[%d]= %d %n", i+1,i,sale[i]);
+		}
+	}
+}
