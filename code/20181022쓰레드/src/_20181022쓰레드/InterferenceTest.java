de64a4217524c589f42c7878f1954410e3ee753a
Tak-kuen <songtak456@naver.com>
2018-10-26 09:02:46 +0900

    181026
    Jsp with Ajax

--- code/20181022쓰레드/src/_20181022쓰레드/InterferenceTest.java
+++ code/20181022쓰레드/src/_20181022쓰레드/InterferenceTest.java
@@ -2,9 +2,12 @@ package _20181022쓰레드;
 
 class Counter {
 	private int value=0;
-	public void increment() { value++;}
-	public void decrement() { value--;}
-	public void printCount() { System.out.println(value); }
+	public synchronized void increment() { value++;}
+	// 접근자와 리턴형태 적는사이에 synchronized 키워드 입력해
+	// 동기화된 메소드 사용
+	// DB에서 RX락 걸리는것과 같은 작용 발생
+	public synchronized void decrement() { value--;}
+	public synchronized void printCount() { System.out.println(value); }
 }
 
 public class InterferenceTest extends Thread{
